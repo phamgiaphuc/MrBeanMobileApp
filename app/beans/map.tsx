3fb5f90777f7d304707bd12c18da05d54bcaea7d
Acus <acus1304pgphcm@gmail.com>
2024-04-05 12:38:02 +0700

    Phuc | Implement map

--- app/beans/map.tsx
+++ app/beans/map.tsx
@@ -1,19 +1,55 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Alert } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+
+const INITIAL_REGION = {
+  latitude: 10.877884,
+  longitude: 106.801526,
+  latitudeDelta: 2,
+  longitudeDelta: 2
+}
+
+const markers = [
+  {
+    id: 1,
+    latitude: 10.877884,
+    longitude: 106.801526,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: 'International University'
+  }
+]
 
 const Page = () => {
+  const onMarkerSelected = (location: any) => {
+    Alert.alert(location.name)
+  }
+
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Stack.Screen 
         options={{
           headerTitle: 'Bean Map',
           headerBackTitle: 'Back'
         }}
       />
-      <Text>Map Page</Text>
+      <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_GOOGLE} initialRegion={INITIAL_REGION} showsUserLocation showsMyLocationButton>
+        {
+          markers.map((location) => (
+            <Marker key={location.id} coordinate={location} onPress={() => onMarkerSelected(location)}/>
+          ))
+        }
+      </MapView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mapStyle:  {
+    width: '100%',
+    height: '100%'
+  }
+})
 
 export default Page
